828c36330c70dbc9acaea01c801bdaeb32f32f45
ekaterinakozlovskaya <katia.k.92@mail.ru>
2023-10-21 20:57:47 +0300

    добавили текст программы

---
+++ 2023/2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int[] NumbersMN(int numM, int numN)
+{
+    if (numM == numN) 
+    {
+        int[] arr = new int[] {numM, numM, numM};
+        return arr;
+    }
+    else if (numM > numN)
+    {
+        int[] arr = new int[] {numN, numM};
+        return arr;
+    }
+    else
+    {
+        int[] arr = new int[] {numM, numN};
+        return arr; 
+    }
+}
+
+int SumNumbers (int min, int max)
+{
+    if (min > max) return 0;
+    else return min += SumNumbers(min + 1, max);
+}
+
+Console.WriteLine("Введите натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int[] numbersMN = NumbersMN(numberM, numberN);
+if (numbersMN.Length > 2)
+{
+    Console.WriteLine($"M = {numberM}; N = {numberN} -> {numbersMN[2]}");
+}
+else 
+{
+    int result = SumNumbers(numbersMN[0], numbersMN[1]);
+    Console.WriteLine($"M = {numberM}; N = {numberN} -> {result}");
+}
